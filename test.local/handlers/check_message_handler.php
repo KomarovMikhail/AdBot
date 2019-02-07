e4be78d1b1799a4b895c96ec4abb4e8d765cf885
Михаил Комаров <mikhailkomarov@MacBook-Air-Mihail.local>
2019-02-08 02:39:16 +0300

    Some more fixes

--- test.local/handlers/check_message_handler.php
+++ test.local/handlers/check_message_handler.php
@@ -1,5 +1,21 @@
 <?php
 
+// checks if message value is in list of properties
+function check_list($message, $property) {
+    $file = fopen("properties/" . $property, "r") or die("Unable to open file!");
+    while(!feof($file)) {
+        $item = fgets($file);
+        $lower_item = mb_strtolower($item);
+        $lower_input = mb_strtolower($message);
+        if ($lower_input !== $lower_item) {
+            fclose($file);
+            return false;
+        }
+    }
+    fclose($file);
+    return true;
+}
+
 $message = $_POST['message'];
 $step = $_POST['step'];
 //$step = 7;
@@ -12,6 +28,30 @@ switch ($step) {
             $result = 0;
         }
         else if (preg_match('/(19|20)[0-9][0-9]/', $message) == 0) {
+            $result = 0;
+        }
+        break;
+    }
+    case 3: {
+        if (!check_list($message, "education")) {
+            $result = 0;
+        }
+        break;
+    }
+    case 2: {
+        if (!check_list($message, "city")) {
+            $result = 0;
+        }
+        break;
+    }
+    case 4: {
+        if (!check_list($message, "university")) {
+            $result = 0;
+        }
+        break;
+    }
+    case 5: {
+        if (!check_list($message, "speciality")) {
             $result = 0;
         }
         break;
@@ -32,6 +72,18 @@ switch ($step) {
             }
         }
         else if (preg_match('/[01234]\.[0-9]+/', $message) == 0) {
+            $result = 0;
+        }
+        break;
+    }
+    case 10: {
+        if (!check_list($message, "speciality")) {
+            $result = 0;
+        }
+        break;
+    }
+    case 11: {
+        if (!check_list($message, "business")) {
             $result = 0;
         }
         break;
